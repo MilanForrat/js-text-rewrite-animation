964800effb1ed281ae9b667137f1a47306b4c762
milan.forrat <milan.forrat@gmail.com>
2021-06-11 14:05:12 +0200

    feat: added tooltips

--- app.js
+++ app.js
@@ -45,3 +45,9 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// bootstrap tooltips: 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
